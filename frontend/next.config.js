57ffb930b450040d4b48ff7269d10e3e88abadb8
YiEnTsai-91 <m951235@gmail.com>
2025-05-06 19:59:52 -0700

    eslint ignore

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,25 +1,12 @@
 const path = require("path");
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     svgr: true,
   },
-  // webpack(config) {
-  //   config.module.rules.unshift({
-  //     test: /\.svg$/,
-  //     issuer: /\.[jt]sx?$/,
-  //     use: ["@svgr/webpack"],
-  //   });
-
-  //   config.module.rules.push({
-  //     test: /\.(png|jpe?g|gif|webp)$/i,
-  //     type: "asset/resource",
-  //   });
-
-  //   config.resolve.alias["@assets"] = path.resolve(__dirname, "src/assets");
-
-  //   return config;
-  // },
 };
 
 module.exports = nextConfig;
--- frontend/types/img.d.ts
+++ frontend/types/img.d.ts
@@ -16,24 +16,3 @@ declare module '*.jpg' {
   const value: string;
   export default value;
 }
-
-// src/types/image.d.ts
-declare module "*.svg?url" {
-  const content: string;
-  export default content;
-}
-
-declare module "*.png?url" {
-  const content: string;
-  export default content;
-}
-
-declare module "*.jpg?url" {
-  const content: string;
-  export default content;
-}
-
-declare module "*.webp?url" {
-  const content: string;
-  export default content;
-}
